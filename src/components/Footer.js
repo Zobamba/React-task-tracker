d6560850a6df29ba8b7e446b8ad45b9b1900df00
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2022-10-17 11:09:15 +0100

    React-task-tracker

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2022 Tasktracker, Inc.</p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
+ 
